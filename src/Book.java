7466c3206e9eca3d63923abc169a329655a39e3c
VladimirChernev <vchernev@abv.bg>
2017-10-31 22:36:44 +0200

    Added Book Type (Class)

---
+++ src/Book.java
@@ -0,0 +1,62 @@
+public class Book {
+
+    private String title;
+    private String author;
+    private double price;
+    private String publishingHouse;
+    private boolean isForeign;
+
+    public Book() {
+    }
+
+    private int stockUnits;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getPublishingHouse() {
+        return publishingHouse;
+    }
+
+    public void setPublishingHouse(String publishingHouse) {
+        this.publishingHouse = publishingHouse;
+    }
+
+    public boolean isForeign() {
+        return isForeign;
+    }
+
+    public void setForeign(boolean foreign) {
+        isForeign = foreign;
+    }
+
+    public int getStockUnits() {
+        return stockUnits;
+    }
+
+    public void setStockUnits(int stockUnits) {
+        this.stockUnits = stockUnits;
+    }
+
+}
